02fb0ff58130a670a7f2994e604f21535a68d831
Arseny Stefanenko <stefisen@yandex.ru>
2025-12-09 09:04:08 +0300

    а

--- dinner-for-two/src/components/AddDishForm.tsx
+++ dinner-for-two/src/components/AddDishForm.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { useState } from 'react'
+import type React from 'react'
 import { addDish, generateDishIngredients, deleteDish } from '@/app/actions'
 import { handleError, createErrorContext } from '@/utils/errorHandler'
 import { Button } from '@/components/ui/button'
@@ -77,7 +78,7 @@ export function AddDishForm({ day, onAdded, onCancel, onRemove }: { day: number,
         autoFocus
         placeholder={t.addDishPlaceholder}
         value={name}
-        onChange={(e) => setName(e.target.value)}
+        onChange={(e: React.ChangeEvent<HTMLInputElement>) => setName(e.target.value)}
         disabled={loading}
         className="bg-card"
       />
